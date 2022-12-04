8ab4ff4085280c2d2e57079de211ec23b200d6d2
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-12-04 23:20:23 +0800

    Remove most of the Default file. Add defaultImages.json to replace them. (after v0.8.0))

--- server.js
+++ server.js
@@ -121,17 +121,12 @@ app.get("/getGameDatas", function (req, res) {
 })
 
 app.get("/getDefaultImgDatas", function (req, res) {
-  const {name,gameId} = req.query;
-  // console.log(" DefaultDatas name: ",name,"  gameId: ",gameId);
-
-  const fileDatas = fs.readFileSync(`./public/gameDatas/${gameId}/${name}Default.json`)
-
+  const fileDatas = fs.readFileSync(`./public/gameDatas/defaultImages.json`)
   res.json(JSON.parse(fileDatas));
 })
 
 app.post("/publishGame", function (req, res) {
   const {gameId,username,gameModifyDatas} = req.query;
-  // console.log();
   let isSuccessed = false 
   
   let userPublishGameModel = mongoose.model(username, userPublishGameSchema)
@@ -154,36 +149,6 @@ app.post("/publishGame", function (req, res) {
     gameUrl: `playGame/?gameId=${gameId}&username=${username}`
   });
 })
-
-// app.get("/search/users", function (req, res) {
-//   const {q} = req.query
-//   axios({
-//     url: 'https://api.github.com/search/users',
-//     params: {q}
-//   }).then(response => {
-//     res.json(response.data)
-//   })
-// })
-
-// app.get("/search/users2", function (req, res) {
-//   res.json({
-//     items: [
-//       {
-//         login: "yyx990803",
-//         html_url: "https://github.com/yyx990803",
-//         avatar_url:
-//           "https://avatars3.githubusercontent.com/u/499550?s=460&u=de41ec9325e8a92e281b96a1514a0fd1cd81ad4a&v=4",
-//         id: 1,
-//       },
-//       {
-//         login: "ruanyf",
-//         html_url: "https://github.com/ruanyf",
-//         avatar_url: "https://avatars2.githubusercontent.com/u/905434?s=460&v=4",
-//         id: 2,
-//       }
-//     ],
-//   });
-// });
 
 app.listen(5050, "0.0.0.0", (err) => {
   if (!err){ console.log("connect 5050")} 
